4d0b3de2ce500238610954f03fc97f1d7421a212
Starchenko Alex <aando.lg@gmail.com>
2019-03-17 17:47:28 +0200

    Added new entity 'book' and relatons to user entity

---
+++ src/entity/book.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Length, IsNotEmpty, IsDate } from 'class-validator';
+import {User} from "./user";
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        length: 80
+    })
+    @Length(0, 80)
+    @IsNotEmpty()
+    name: string;
+
+    @Column({
+        length: 100
+    })
+    description: string;
+
+    @Column({default: () => `now()`})
+    @IsDate()
+    date: string
+
+    @ManyToOne(type => User, user => user.books)
+    user: User;
+}
